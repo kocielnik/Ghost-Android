ab1fc75ca5d6248fd9f932d095ba4b128f1321e8
Patryk Kocielnik <patryk@kocielnik.pl>
2026-02-19 14:40:38 +0100

    Fix authentication.

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: token $GITHUB_TOKEN" \
+  -H 'authorization: Bearer ${{ secrets.GITHUB_TOKEN }}' \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/kocielnik/Ghost-Android/releases \
   -d "{'tag_name':'$VERSION','target_commitish':'ci','name':'$VERSION','body':'Ghost Android v. $VERSION','draft':true,'prerelease':false,'generate_release_notes':false}"
